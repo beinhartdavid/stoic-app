0c81db25df49a595ca38b0cd2daeec1f6a0b08df
beinhartdavid <beinhart.david@gmail.com>
2020-01-17 18:15:52 -0500

    Getting Rexex Ready

---
+++ process-text/text-to-json.py
@@ -0,0 +1,20 @@
+"""
+This file will read in the txt file of the Daily Stoic from /data/DailyStoicText.txt and will use regex files to convert
+it to JSON
+"""
+
+# Function 1 Read txt file
+
+def readTxt(file):
+    '''open TXT
+    Loop through every line
+    Identify each:
+    a. Date
+    b. Wisdom
+    c. Quote
+    d. Speaker
+    e. Analysis
+    '''
+
+    return "Hello World"
+
